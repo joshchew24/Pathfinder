8d32251125f696754a4f8e6ae1023ea91567ffb7
AllanJiang17 <allanjiang17@gmail.com>
2024-03-21 00:32:34 -0700

    fixed position of texture and modified parallax factor

--- shaders/background.vs.glsl
+++ shaders/background.vs.glsl
@@ -7,9 +7,12 @@ out vec2 TexCoord;
 
 uniform vec2 camera;
 uniform float parallaxFactor;
+uniform vec3 transform;
 
 void main() {
     vec2 adjustedTexCoord = texCoord - camera * vec2(parallaxFactor);
-    gl_Position = vec4(position, 1.0);
+    vec3 newPosition = position + transform;
+
+    gl_Position = vec4(newPosition, 1.0);
     TexCoord = adjustedTexCoord;
 }
--- src/render_system_init.cpp
+++ src/render_system_init.cpp
@@ -324,24 +324,19 @@ void RenderSystem::initParallaxRendering() {
 	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f
 	};
 
-	// Generate and bind VAO
 	glGenVertexArrays(1, &backGroundVao);
 	glBindVertexArray(backGroundVao);
 
-	// Generate and bind VBO
 	glGenBuffers(1, &backGroundVbo);
 	glBindBuffer(GL_ARRAY_BUFFER, backGroundVbo);
 
-	// Set vertex data
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Specify attribute pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	// Unbind VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
