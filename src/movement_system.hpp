f2358fdd1db7ad10d542f29f510865d3d1c4eb44
Joshua <joshuagchew@gmail.com>
2024-03-22 16:05:30 -0700

    config loader for move_speed

--- src/movement_system.hpp
+++ src/movement_system.hpp
@@ -15,6 +15,7 @@ private:
 	bool right = false;
 	int last = 0;
 	float friction;
+	float move_speed;
 
 public:
 	const static int LEFT_KEY = GLFW_KEY_A;
@@ -22,9 +23,9 @@ public:
 
 	MovementSystem() {}
 
-	void init(float friction_arg = config.friction) {
-		printf("%f\n", friction_arg);
+	void init(float friction_arg = config.friction, float move_speed_arg = config.move_speed) {
 		friction = friction_arg;
+		move_speed = move_speed_arg;
 	}
 
 	void press(int key) {
@@ -78,16 +79,15 @@ public:
 		if (last == 0) return;
 		Motion& motion = registry.motions.get(registry.players.entities[0]);
 		motion.acceleration.x = 0.0;
-		float vel = 250.f;
 		if (last == LEFT_KEY && left) {
-			motion.velocity.x = vel * -1.f;
+			motion.velocity.x = move_speed * -1.f;
 			if (motion.scale.x > 0) {
 				motion.scale.x = -motion.scale.x;
 			}
 			return;
 		}
 		if (last == RIGHT_KEY && right) {
-			motion.velocity.x = vel;
+			motion.velocity.x = move_speed;
 			if (motion.scale.x < 0) {
 				motion.scale.x = -motion.scale.x;
 			}
