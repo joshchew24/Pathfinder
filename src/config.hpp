b1ff83fd60ef91b9a78ebd7b65eb48972c5dce86
Joshua <joshuagchew@gmail.com>
2024-03-21 22:23:01 -0700

    add 4 configurable variables

--- src/config.hpp
+++ src/config.hpp
@@ -1,11 +1,19 @@
 #include "common.hpp"
 
 class Config {
-private:
-	json config;
 public:
+	float gravity;
+	float move_speed;
+	float jump_height;
+	float friction;
+
 	void load(std::string file_name="config.json") {
 		std::ifstream file(data_path() + "/" + file_name);
-		config = json::parse(file);
+		json config = json::parse(file);
+
+		gravity = config["gravity"];
+		move_speed = config["move_speed"];
+		jump_height = config["jump_height"];
+		friction = config["friction"];
 	}
 };
