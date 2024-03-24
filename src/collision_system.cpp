e276cebd6162c9225d5ca1ff44253a6f0ed11ce3
Brayden Shinkawa <braydenshinkawa@gmail.com>
2024-03-24 13:29:10 -0700

    refactored physics system collision components into a collision system

---
+++ src/collision_system.cpp
@@ -0,0 +1,237 @@
+#include <collision_system.hpp>
+#include "world_init.hpp"
+
+
+// Returns the local bounding coordinates scaled by the current size of the entity
+vec2 get_bounding_box(const Motion& motion)
+{
+	// abs is to avoid negative scale due to the facing direction.
+	return { abs(motion.scale.x), abs(motion.scale.y) };
+}
+
+bool CollisionSystem::collides(const Motion& motion1, const Entity& entity1, const Motion& motion2, const Entity& entity2)
+{
+	if (registry.renderRequests.has(entity1) && registry.renderRequests.get(entity1).used_geometry != GEOMETRY_BUFFER_ID::GEOMETRY_COUNT &&
+		registry.meshPtrs.has(entity1) && registry.meshPtrs.get(entity1)->vertices.size() > 0 && SATcollision(registry.meshPtrs.get(entity1), motion1, motion2)) {
+		return true;
+	}
+	else if (registry.renderRequests.has(entity2) && registry.renderRequests.get(entity2).used_geometry != GEOMETRY_BUFFER_ID::GEOMETRY_COUNT &&
+	registry.meshPtrs.has(entity2) && registry.meshPtrs.get(entity2)->vertices.size() > 0 && SATcollision(registry.meshPtrs.get(entity2), motion2, motion1)) {
+		return true;
+	}
+	else if (rectangleCollides(motion1, motion2)) {
+		return true;
+	}
+	return false;
+}
+
+ColoredVertex v0;
+
+// find vertex next to top in stack
+ColoredVertex nextToTop(std::stack<ColoredVertex>& S) {
+	ColoredVertex v = S.top();
+	S.pop();
+	ColoredVertex res = S.top();
+	S.push(v);
+	return res;
+}
+
+// swap positions in vector array
+void swap(ColoredVertex& v1, ColoredVertex& v2) {
+	ColoredVertex temp = v1;
+	v1 = v2;
+	v2 = temp;
+}
+
+// find distance between two points
+float distSq(vec3 p1, vec3 p2) {
+	return (p1.x - p2.x) * (p1.x - p2.x) +
+		(p1.y - p2.y) * (p1.y - p2.y);
+}
+
+// find orientation of triplet vertices, where 0 == collinear, 1 == clockwise, 2 == counterclockwise
+int orientation(vec3 p, vec3 q, vec3 r) {
+	float threshold = 1e-5;
+	float val = (q.y - p.y) * (r.x - q.x) -
+		(q.x - p.x) * (r.y - q.y);
+	if (abs(val) <= threshold) return 0; // collinear
+	return (val > 0) ? 1 : 2;
+}
+
+int compare(const void* vp1, const void* vp2) {
+	ColoredVertex* v1 = (ColoredVertex*)vp1;
+	ColoredVertex* v2 = (ColoredVertex*)vp2;
+
+	// Find orientation
+	int orient = orientation(v0.position, v1->position, v2->position);
+	if (orient == 0)
+		return (distSq(v0.position, v2->position) >= distSq(v0.position, v1->position)) ? -1 : 1;
+
+	return (orient == 2) ? -1 : 1;
+}
+
+// Graham Scan for generating convex hull (and avoid concave mesh shapes)
+// reference code: https://www.geeksforgeeks.org/convex-hull-using-graham-scan/
+// changed so graham scan works on float values: orientation function now uses threshold instead of absolute 0 value
+std::vector<ColoredVertex> convexHull(std::vector<ColoredVertex> vertices, int n) {
+	std::vector<ColoredVertex> res = std::vector<ColoredVertex>();
+	float ymin = vertices[0].position.y;
+	int indexMin = 0;
+	for (int i = 1; i < n; i++) {
+		// find bottom most point
+		float y = vertices[i].position.y;
+		if ((y < ymin) || ymin == y && vertices[i].position.x < vertices[indexMin].position.x) {
+			ymin = vertices[i].position.y;
+			indexMin = i;
+		}
+	}
+
+	// put bottom-most point at first position
+	swap(vertices[0], vertices[indexMin]);
+	v0 = vertices[0];
+	int n_minus = n - 1;
+	qsort(&vertices[1], n_minus, sizeof(ColoredVertex), compare);
+	// if two or more points make the same angle with p0, remove all but the farthest from p0
+	int m = 1;
+	for (int i = 1; i < n; i++) {
+		// skip i_th vertice while angle of i and i+1 is same with respect to p0
+		int j = i + 1;
+		while (i < n - 1 && orientation(v0.position, vertices[i].position, vertices[j].position) == 0) {
+			i++;
+		}
+		vertices[m] = vertices[i];
+		m++; // update size of modified array
+	}
+	if (m < 3) return std::vector<ColoredVertex>(); // return empty if less than 3 vertices -- not an object
+
+	std::stack<ColoredVertex> S;
+	S.push(vertices[0]);
+	S.push(vertices[1]);
+	S.push(vertices[2]);
+
+	// process remaining points
+	for (int i = 3; i < m; i++) {
+		// remove top while angle formed by points next to top, top, and res[i] makes a non-left turn
+		while (S.size() > 1 && orientation(nextToTop(S).position, S.top().position, vertices[i].position) != 2)
+			S.pop();
+		S.push(vertices[i]);
+	}
+
+	// check output points
+	while (!S.empty()) {
+		ColoredVertex v = S.top();
+		res.push_back(v);
+		S.pop();
+	}
+	return res;
+}
+
+void normalizeProj(vec2& proj) {
+	float magnitude = sqrt(proj.x * proj.x + proj.y * proj.y);
+	if (magnitude > 1e-9) {
+		proj.x /= magnitude;
+		proj.y /= magnitude;
+	}
+}
+
+// Separating Axis Theorem between mesh and motion entity
+// modified so that the vertice positions would be translated, rotated, and scaled beforehand
+bool CollisionSystem::SATcollision(const Mesh* mesh, const Motion& motion1, const Motion& motion2) {
+	float bot = motion2.position.y + abs(motion2.scale.y) / 2.f;
+	float top = motion2.position.y - abs(motion2.scale.y) / 2.f;
+	float right = motion2.position.x + abs(motion2.scale.x) / 2.f;
+	float left = motion2.position.x - abs(motion2.scale.x) / 2.f;
+	std::vector<ColoredVertex> vertices = convexHull(mesh->vertices, mesh->vertices.size());
+	std::vector<vec2> trsPositions = std::vector<vec2>();
+	// modify positions so they're in the right spots
+	for (int i = 0; i < vertices.size(); i++) {
+		trsPositions.push_back(translateRotateScale(vertices[i].position, motion1));
+	}
+	for (int s = 0; s < 2; s++) {
+		if (s == 0) {
+			// get projections from mesh
+			for (int i = 0; i < trsPositions.size(); i++) {
+				int j = i + 1;
+				j = j % trsPositions.size();
+				vec2 proj = { -(trsPositions[j].y - trsPositions[i].y), trsPositions[j].x - trsPositions[i].x };
+				normalizeProj(proj);
+				float min_r1 = INFINITY, max_r1 = -INFINITY;
+				for (int p = 0; p < trsPositions.size(); p++) {
+					float val = (trsPositions[p].x * proj.x + trsPositions[p].y * proj.y);
+					min_r1 = min(min_r1, val);
+					max_r1 = max(max_r1, val);
+				}
+
+				float min_r2 = INFINITY, max_r2 = -INFINITY;
+
+				float topLeft = top * proj.y + left * proj.x;
+				float topRight = top * proj.y + right * proj.x;
+				float botLeft = bot * proj.y + left * proj.x;
+				float botRight = bot * proj.y + right * proj.x;
+				min_r2 = min(min_r2, topLeft);
+				min_r2 = min(min_r2, topRight);
+				min_r2 = min(min_r2, botLeft);
+				min_r2 = min(min_r2, botRight);
+
+				max_r2 = max(max_r2, topLeft);
+				max_r2 = max(max_r2, topRight);
+				max_r2 = max(max_r2, botLeft);
+				max_r2 = max(max_r2, botRight);
+
+				if (!(max_r2 >= min_r1 && max_r1 >= min_r2)) {
+					return false;
+				}
+			}
+		}
+		// get projections from rectangle
+		else {
+			// case 1: projection onto x-axis
+			for (int i = 0; i < 2; i++) {
+				vec2 proj = { 1, 0 };
+
+				// case 2: projection onto y-axis
+				if (i == 1) {
+					proj = { 0, 1 };
+				}
+
+				float min_r1 = FLT_MAX, max_r1 = FLT_MIN;
+				for (int p = 0; p < trsPositions.size(); p++) {
+					float val = (trsPositions[p].x * proj.x + trsPositions[p].y * proj.y);
+					min_r1 = min(min_r1, val);
+					max_r1 = max(max_r1, val);
+				}
+
+				float min_r2 = FLT_MAX, max_r2 = FLT_MIN;
+
+				// do top left
+				float topLeft = top * proj.y + left * proj.x;
+				float topRight = top * proj.y + right * proj.x;
+				float botLeft = bot * proj.y + left * proj.x;
+				float botRight = bot * proj.y + right * proj.x;
+				min_r2 = min(min_r2, topLeft);
+				min_r2 = min(min_r2, topRight);
+				min_r2 = min(min_r2, botLeft);
+				min_r2 = min(min_r2, botRight);
+
+				max_r2 = max(max_r2, topLeft);
+				max_r2 = max(max_r2, topRight);
+				max_r2 = max(max_r2, botLeft);
+				max_r2 = max(max_r2, botRight);
+
+
+				if (!(max_r2 >= min_r1 && max_r1 >= min_r2)) {
+					return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+bool CollisionSystem::rectangleCollides(const Motion& motion1, const Motion& motion2) {
+	bool y_val = (motion1.position[1] - abs(motion1.scale.y) / 2.f) < (motion2.position[1] + abs(motion2.scale.y) / 2.f) &&
+		(motion2.position[1] - abs(motion2.scale.y) / 2.f) < (motion1.position[1] + abs(motion1.scale.y) / 2.f);
+	bool x_val = (motion1.position[0] - abs(motion1.scale.x / 2.f)) < (motion2.position[0] + abs(motion2.scale.x) / 2.f) &&
+		(motion2.position[0] - abs(motion2.scale.x / 2.f) < (motion1.position[0] + abs(motion1.scale.x) / 2.f));
+	return y_val && x_val;
+}
